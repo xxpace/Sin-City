4476f51e37339882650c7471c8b1d408c141bd23
xxpace <357860490@qq.com>
2017-06-28 22:38:59 +0800

    test mysql

--- server/game-server/app/test.js
+++ server/game-server/app/test.js
@@ -1,16 +1,36 @@
-var Card = require('./ddz_domain/card.js').Card;
-var StyleJudge = require('./ddz_domain/card.js').StyleJudge;
-var CardsProxy = require('./ddz_domain/card.js').CardsProxy;
+// var Card = require('./ddz_domain/card.js').Card;
+// var StyleJudge = require('./ddz_domain/card.js').StyleJudge;
+// var CardsProxy = require('./ddz_domain/card.js').CardsProxy;
 
-let card = new Card(1,1,1);
+// let card = new Card(1,1,1);
 
-let cardsPorxy = new CardsProxy();
-cardsPorxy.initCards();
-cardsPorxy.upsetCards();
+// let cardsPorxy = new CardsProxy();
+// cardsPorxy.initCards();
+// cardsPorxy.upsetCards();
 
-let oneStyle = cardsPorxy.styleJudge.getCardStyle([cardsPorxy.cardPool[0]]);
-let twoStyle = cardsPorxy.styleJudge.getCardStyle([cardsPorxy.cardPool[1]]);
+// let oneStyle = cardsPorxy.styleJudge.getCardStyle([cardsPorxy.cardPool[0]]);
+// let twoStyle = cardsPorxy.styleJudge.getCardStyle([cardsPorxy.cardPool[1]]);
 
-let result = cardsPorxy.compareCards([cardsPorxy.cardPool[1]],twoStyle,[cardsPorxy.cardPool[0]],oneStyle);
+// let result = cardsPorxy.compareCards([cardsPorxy.cardPool[1]],twoStyle,[cardsPorxy.cardPool[0]],oneStyle);
 
-console.log(result);
+// console.log(result);
+
+
+//test mysql
+var mysql = require('mysql');
+
+var client = mysql.createConnection({
+                					host:"127.0.0.1",
+                					user:"root",
+               						password:"147258",
+                					database:"game"
+            						});
+client.connect();
+
+var sql = 'select username from user where id = ?';
+var args = [1];
+client.query(sql,args,function(err,res){
+	if(err)throw err;
+	console.log(res[0]);
+});
+// client.end();
