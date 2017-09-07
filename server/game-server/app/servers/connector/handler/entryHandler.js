871f814aeb6d35c9146c1fc62aa1dcbe29ccf571
xxpace <357860490@qq.com>
2017-09-07 17:51:55 +0800

    删除没用的注释

--- server/game-server/app/servers/connector/handler/entryHandler.js
+++ server/game-server/app/servers/connector/handler/entryHandler.js
@@ -7,32 +7,15 @@ var Handler = function(app) {
   this.idCount = 0;
 };
 
-/**
- * New client entry.
- *
- * @param  {Object}   msg     request message
- * @param  {Object}   session current session object
- * @param  {Function} next    next step callback
- * @return {Void}
- */
 Handler.prototype.entry = function(msg, session, next) {
 	var self = this;
 	var uid = "user*"+self.idCount;
 	self.idCount++;
 	session.bind(uid);
 	session.on('closed', onUserLeave.bind(null, self.app));
-	// self.app.rpc.ddz.ddzRemote.add(session,uid,self.app.get('serverId'),true,function(roomid){
-	// 	session.set('rid', roomid);
-	// 	session.push('rid', function(err) {
-	// 		if(err) {
-	// 			console.error('set rid for session service failed! error is : %j', err.stack);
-	// 		}
-	// 	});
-	// 	next(null);
-	// });
 	self.app.rpc.lobby.lobbyRemote.entryLobby(session,uid,function(playerInfo){
 		next(null,playerInfo);
-	})
+	});
 };
 
 var onUserLeave = function(app, session) {
